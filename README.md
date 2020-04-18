be10944256565e42b380ce2bc7e724d4b284b4fa
Ruben Giannotti <ruben.giannotti@gmx.net>
2020-04-18 15:50:00 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,165 @@
+# Type insurance
+
+> Helper class to force (input) types, mainly for pure JavaScript environments
+
+## Key notes / highlights
+
+- Multi type container
+- Ensures a type for a variable declaration (in opposition to type checking)
+- e.g. when using the type 'any'
+- Tailored for non type sensitive environments
+- Useful when working with uncertain data from third party API's
+- Returns actually `false` for empty objects (or any other implicitely falsy value)
+- Simplistic approach
+- Doesn't extend the built in `.prototype`s
+- Clean and focused
+
+## Install
+
+```console
+$ npm install type-insurance
+```
+
+## Usage
+
+```js
+import TypeInsurance from 'type-insurance';
+// or
+//const TypeInsurance = require('type-insurance');
+
+const input = new TypeInsurance('foo');
+
+console.log(input.string);  // "foo" 
+console.log(input.number);  // 68123873083688143418383284816464454849230703155
+console.log(input.boolean); // true
+console.log(input.array);   // ["foo"]
+console.log(input.object);  // { key: "foo" }
+
+const arr = new TypeInsurance([1, 2, 3]);
+
+console.log(arr.string);  // "[1, 2, 3]" 
+console.log(arr.number);  // 6
+console.log(arr.boolean); // true
+console.log(arr.array);   // [1, 2, 3]
+console.log(arr.object);  // { 0: 1, 1: 2, 3: 2 }
+```
+
+## Assumptions
+
+"Object", hereinafter is understood as a "real" object, meaning the the intersection of the sets "objects" and "not arrays".
+
+All inputs should return themselves when requesting its original type. And all falsy values except `false` should map to
+
+- `''` (string)
+- `0` (number)
+- `false` (boolean)
+- `[]` (array)
+- `{}` (object)
+
+The inputs `[]` and `{}` should be treated as falsy inputs.
+
+A non-empty string should map to
+
+- The decimal representation of the sha1 conversion (number)
+- `true` (bool)
+- An array containing the string (array)
+- An object with the default key and the string as the value (object)
+
+where the edge case `'false'` should return `false` when requesting the `.boolean` property. Numbers shall work in an analogous fashion. `0` especially should yield `false`.
+
+Respectively, `true|false` should return the stringified versions, `1|0` and `[true|false]`.
+
+## API
+
+### `new TypeInsurance(input, [options])`
+
+Example: `const myInput = new TypeInsurance(myParam);`
+
+A class instance of `TypeInsurnace` holds the properties `.string`, `.number`, `.boolean`, `.array` and `.object` holding the accordingly typed values.
+
+Options get passed in as an object. Available options are:
+
+- `defaultKey` (default: "key") - Specifies the default key for implicitely generated objects from strings and numbers
+
+#### .string
+
+Returns a string generated from the input of the constructor.
+
+| Input type | Output |
+| :---:   | :--- |
+| string  | unchanged `input` |
+| number  | Series of stringified digits |
+| boolean | `'true'` / `'false'` |
+| array   | Stringified version of the array content |
+| array   | Stringified version of the object content |
+
+#### .number
+
+Returns a number generated from the input of the constructor.
+
+| Input type | Output |
+| :---:   | :--- |
+| string  | SHA-1 converted to decimal |
+| number  | unchanged `input` |
+| boolean | `1` / `0` |
+| array   | Sum of all elemets |
+| array   | Sum of all object values |
+
+#### .boolean
+
+Returns a number generated from the input of the constructor.
+
+| Input type | Output |
+| :---:   | :--- |
+| string  | `true` if `input` is non-empty |
+| number  | `false` if `input === 0` |
+| boolean | unchanged `input` |
+| array   | `false` if `input === []` |
+| array   | `false` if `input === {}` |
+
+#### .array
+
+Returns an array generated from the input of the constructor.
+
+| Input type | Output |
+| :---:   | :--- |
+| string  | Array containing `input` |
+| number  | *see above line* |
+| boolean | *see above line* |
+| array   | unchanged `input` |
+| array   | `Object.values(input)` |
+
+#### .object
+
+Returns an array generated from the input of the constructor.
+
+| Input type | Output |
+| :---:   | :--- |
+| string  | Object containing the key value pair *[defaultKey]=`input`* |
+| number  | *see above line* |
+| boolean | *see above line* |
+| array   | `Object.values(input)` |
+| array   | unchanged `input` |
+
+## Keywords
+
+- data
+- interface
+- type
+- types
+- convert
+- safety
+
+## Dependencies
+
+- [lodash](https://www.npmjs.com/package/lodash)
+- [hash.js](https://www.npmjs.com/package/hash.js)
+- [baseconvert](https://www.npmjs.com/package/baseconvert)
+
+## Related
+
+- [typeablejs](https://github.com/xpepermint/typeablejs) - A library for checking and casting types.
+
+## Maintainer
+
+- [Ruben Giannotti](http://rubengiannotti.com) - ruben.giannotti@gmx.net - github.com/giannotr
--- src/index.js
+++ src/index.js
@@ -1,126 +1,115 @@
-//import hash from 'hash.js';
-//import objectHash from 'object-hash';
-//import baseConvert from 'baseconvert';
-const hash = require('hash.js');
-//const objectHash = require('object-hash');
-const baseConvert = require('baseconvert');
-const isEmpty = require('lodash/isEmpty');
+import hash from 'hash.js';
+import baseConvert from 'baseconvert';
+import isEmpty from 'lodash/isEmpty';
 
 function castString(input) {
-  const isArray = Array.isArray(input);
+	const isArray = Array.isArray(input);
 
-  if(!input && typeof input === 'boolean') {
-    return 'false';
-  } else if(input === 0) {
-    return '0';
-  } else if(input && typeof input === 'object') {
-    if(isEmpty(input)) {
-      return '';
-    } else {
-      if(isArray) {
-        return `[${input.join(', ')}]`;
-      } else if(!isArray) {
-        const values = Object.values(input);
-        const entries = Object.keys(input).map((k, i) => [k, values[i]]);
-        return `{${entries.map(e => e.join(': ')).join(', ')}}`;
-      }
-    }
-  } else {
-    return input ? ''+input : '';
-  }
+	if(!input && typeof input === 'boolean') {
+		return 'false';
+	} else if(input === 0) {
+		return '0';
+	} else if(input && typeof input === 'object') {
+		if(isEmpty(input)) {
+			return '';
+		} else {
+			if(isArray) {
+				return `[${input.join(', ')}]`;
+			} else if(!isArray) {
+				const values = Object.values(input);
+				const entries = Object.keys(input).map((k, i) => [k, values[i]]);
+				return `{${entries.map(entry => entry.join(': ')).join(', ')}}`;
+			}
+		}
+	} else {
+		return input ? '' + input : '';
+	}
 }
 
-//console.log(castString({ a: 1, b: 2, c: 3 }));
-
 function castNumber(input) {
-  const isArray = Array.isArray(input);
+	const isArray = Array.isArray(input);
 
-  if(typeof input === 'string' && input) {
-    return baseConvert.hex2dec(hash.sha1().update(input).digest('hex'));
-  } else if(isArray) {
-    return input.reduce((a, b) => castNumber(a) + castNumber(b), 0);
-  } else if(input && typeof input === 'object' && !isArray) {
-    return castNumber(Object.values(input));
-  } else {
-    return input ? +input : 0;
-  }
+	if(typeof input === 'string' && input) {
+		return baseConvert.hex2dec(hash.sha1().update(input).digest('hex'));
+	} else if(isArray) {
+		return input.reduce((a, b) => castNumber(a) + castNumber(b), 0);
+	} else if(input && typeof input === 'object' && !isArray) {
+		return castNumber(Object.values(input));
+	} else {
+		return input ? +input : 0;
+	}
 }
 
 function castBoolean(input) {
-  if(typeof input === 'object' && isEmpty(input)) {
-    return false;
-  } else {
-    return !!input;
-  }
+	if(typeof input === 'object' && isEmpty(input)) {
+		return false;
+	} else {
+		return !!input;
+	}
 }
 
 function castArray(input) {
-  if(Array.isArray(input)) {
-    return input;
-  } else if(typeof input === 'object' && input) {
-    return Object.values(input);
-  } else if(input || typeof input === 'boolean' || input === 0) {
-    return [input];
-  } else {
-    return [];
-  }
+	if(Array.isArray(input)) {
+		return input;
+	} else if(typeof input === 'object' && input) {
+		return Object.values(input);
+	} else if(input || typeof input === 'boolean' || input === 0) {
+		return [input];
+	} else {
+		return [];
+	}
 }
 
 function castObject(input, defaultKey) {
-  const isArray = Array.isArray(input);
+	const isArray = Array.isArray(input);
 
-  if(!(input || typeof input === 'boolean' || input === 0)) {
-    return {};
-  } else if(typeof input === 'object' && !isArray) {
-    return input;
-  } else if(isArray) {
-    return {...input};
-  } else {
-    return { [defaultKey]: input };
-  }
+	if(!(input || typeof input === 'boolean' || input === 0)) {
+		return {};
+	} else if(typeof input === 'object' && !isArray) {
+		return input;
+	} else if(isArray) {
+		return {...input};
+	} else {
+		return {[defaultKey]: input};
+	}
 }
 
-class TypeInsurance {
-  constructor(input, options = {}) {
-    this.input = input;
+export default class TypeInsurance {
+	constructor(input, options = {}) {
+		this.input = input;
 
-    this.defaultKey = options.hasOwnProperty('defaultKey') ?
-      castString(options.defaultKey, { hash: true }) : 'key';
-    this.hashedObjects = options.hasOwnProperty('hashedObjects') ?
-      castBoolean(options.hashedObjects) : false;
+		this.defaultKey = options.hasOwnProperty('defaultKey') ?
+			castString(options.defaultKey, {hash: true}) : 'key';
 
-    this.string = this._string();
-    this.number = this._number();
-    this.boolean = this._boolean();
-    this.array = this._array();
-    this.object = this._object();
-  }
-  
-  _string() {
-    const { input } = this;
-    return castString(input);
-  }
-  
-  _number() {
-    const { input } = this;
-    return castNumber(input);
-  }
-  
-  _boolean() {
-    const { input } = this;
-    return castBoolean(input);
-  }
-  
-  _array() {
-    const { input } = this;
-    return castArray(input);
-  }
-  
-  _object() {
-    const { input, defaultKey } = this;
-    return castObject(input, defaultKey);
-  }
+		this.string = this._string();
+		this.number = this._number();
+		this.boolean = this._boolean();
+		this.array = this._array();
+		this.object = this._object();
+	}
+
+	_string() {
+		const {input} = this;
+		return castString(input);
+	}
+
+	_number() {
+		const {input} = this;
+		return castNumber(input);
+	}
+
+	_boolean() {
+		const {input} = this;
+		return castBoolean(input);
+	}
+
+	_array() {
+		const {input} = this;
+		return castArray(input);
+	}
+
+	_object() {
+		const {input, defaultKey} = this;
+		return castObject(input, defaultKey);
+	}
 }
-
-//export default TypeInsurance;
-module.exports = TypeInsurance;
